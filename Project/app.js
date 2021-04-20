c672fff4b820fc0eee55d22c6e4141d0a7a2a2d5
Rasul06 <kochkarov.001.rasul@gmail.com>
2021-04-20 22:57:24 +0200

    app.js created npm start works mongodb added modules added

---
+++ Project/app.js
@@ -0,0 +1,12 @@
+const express= require(id: 'express');
+const mongoose= require(id: 'mongoose');
+
+const app= express();
+
+
+
+app.listen(3000,() =>
+    {
+        console.log("Server is running on port number 3000");
+    }
+);
