02f123ac8361d3514475f4ebc442852e08112c5b
Rasul06 <kochkarov.001.rasul@gmail.com>
2021-04-26 12:41:15 +0200

    downloaded bootstrap, mongoDB connected, system views some changes...

--- Project/app.js
+++ Project/app.js
@@ -1,7 +1,35 @@
 const express= require(  'express');
 const mongoose= require(  'mongoose');
+const path= require('path');
+const hbs= require('express-handlebars')
 
 const app= express();
+
+
+
+/*  Mongoose to MongoDB*/
+mongoose.connect('mongodb://localhost:27017/cms_project', {useNewUrlParser: true})
+    .then(response =>{
+        console.log("MongoDB connected!");
+    })
+    .catch(err =>{console.log("Connection failed");});
+
+
+/*configure express */
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/* Setup view engine*/
+app.engine('handlebars',hbs({defaultLayout:'default'}));
+app.set('view engine', 'handlebars');
+
+/*  Routes*/
+app.use(  '/', (req, res) =>
+{
+    res.send("Welcome to the app");
+});
 
 
 
