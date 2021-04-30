e7658f87f0155273dcc21737d2466e5277b837b7
Rasul06 <kochkarov.001.rasul@gmail.com>
2021-04-30 15:28:27 +0200

    app.js new changes

--- Project/app.js
+++ Project/app.js
@@ -1,40 +1,73 @@
-const express= require(  'express');
-const mongoose= require(  'mongoose');
-const path= require('path');
-const hbs= require('express-handlebars')
+/* Importing Different Modules */
 
-const app= express();
+const {globalVariables} = require('./config/configuration');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const hbs = require('express-handlebars');
+const {mongoDbUrl, PORT} = require('./config/configuration');
+const flash = require('connect-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const {selectOption} = require('./config/customFunctions');
+const fileUpload = require('express-fileupload');
+
+
+const app = express();
+
+
+// Configure Mongoose to Connect to MongoDB
+mongoose.connect(mongoDbUrl, { useNewUrlParser: true })
+    .then(response => {
+        console.log("MongoDB Connected Successfully.");
+    }).catch(err => {
+    console.log("Database connection failed.");
+});
 
 
 
-/*  Mongoose to MongoDB*/
-mongoose.connect('mongodb://localhost:27017/cms_project', {useNewUrlParser: true})
-    .then(response =>{
-        console.log("MongoDB connected!");
-    })
-    .catch(err =>{console.log("Connection failed");});
-
-
-/*configure express */
+/* Configure express*/
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-/* Setup view engine*/
-app.engine('handlebars',hbs({defaultLayout:'default'}));
-app.set('view engine', 'handlebars');
+/*  Flash and Session*/
+app.use(session({
+    secret: 'anysecret',
+    saveUninitialized: true,
+    resave: true
+}));
 
-/*  Routes*/
-app.use(  '/', (req, res) =>
-{
-    res.send("Welcome to the app");
+app.use(flash());
+
+
+/* Use Global Variables */
+app.use(globalVariables);
+
+
+/* File Upload Middleware*/
+app.use(fileUpload());
+
+/* Setup View Engine To Use Handlebars */
+app.engine('handlebars', hbs({defaultLayout: 'default', helpers: {select: selectOption}}));
+app.set('view engine' , 'handlebars');
+
+
+/* Method Override Middleware*/
+app.use(methodOverride('newMethod'));
+
+
+/* Routes */
+const defaultRoutes = require('./routes/defaultRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
+app.use('/', defaultRoutes);
+app.use('/admin', adminRoutes);
+
+
+/* Start The Server */
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-app.listen(3000,() =>
-    {
-        console.log("Server is running on port number 3000");
-    }
-);
