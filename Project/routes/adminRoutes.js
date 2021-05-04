2242c956eb30e7c964ce0c593cf153623a463502
SHIYI WAN <whw13970737135@gmai.com>
2021-05-04 08:02:25 +0200

    various admin post endpoints

--- Project/routes/adminRoutes.js
+++ Project/routes/adminRoutes.js
@@ -14,3 +14,22 @@ router.all('/*', (req, res, next) => {
 
 router.route('/')
     .get(adminController.index);
+
+router.route('/posts')
+    .get(adminController.getPosts);
+
+
+
+router.route('/posts/create')
+    .get(adminController.createPostsGet)
+    .post(adminController.submitPosts);
+
+
+router.route('/posts/edit/:id')
+    .get(adminController.editPostGetRoute)
+    .put(adminController.editPostUpdateRoute);
+
+
+router.route('/posts/delete/:id')
+    .delete(adminController.deletePost);
+
