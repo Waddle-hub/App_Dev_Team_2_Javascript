66059714ff6548447656a1a28e9c1afbb6692b68
Rasul06 <kochkarov.001.rasul@gmail.com>
2021-05-12 19:31:14 +0200

    some changes made

--- Project/config/configuration.js
+++ Project/config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoDbUrl : 'mongodb://localhost:27017/tutorial_cms',
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 8080,
     globalVariables: (req, res, next) => {
         res.locals.success_message = req.flash('success-message');
         res.locals.error_message = req.flash('error-message');
